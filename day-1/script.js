76c66109271ffca46f53b3db5a54418459b31777
Dawid Ossowski <ossowski.dawid@gmail.com>
2021-04-05 20:37:41 +0200

    trening operatora warunkowego

--- day-1/script.js
+++ day-1/script.js
@@ -17,3 +17,10 @@ switch (dayOfTheWeek) {
         console.log("Nie mam pojęcia jaki jest dzisiaj dzień :(");
         break;
 }
+
+const score = 100;
+20 === "20" && score ? console.log("wykonuje się, bo prawda") : console.log("wykonuje się bo nie jest prawidzy");
+
+false || true ? console.log("prawda") : console.log("fałsz");
+
+score > 120 ? console.log("prawda1") : console.log("fałsz2");
