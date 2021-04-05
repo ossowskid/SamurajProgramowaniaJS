bb4e70b9fcf1f3a5efde085819416bcc157129b8
Dawid Ossowski <ossowski.dawid@gmail.com>
2021-04-05 20:30:50 +0200

    training switch

---
+++ day-1/script.js
@@ -0,0 +1,19 @@
+const dayOfTheWeek = "poniedziałek";
+
+switch (dayOfTheWeek) {
+    case "poniedziałek":
+        console.log("Dzis jest poniedziałek");
+        break;
+    case "wtorek":
+        console.log("Dziś jest wtotek");
+        break;
+    case "środa":
+        console.log("Dziś jest środa");
+        break;
+    case "czwartek":
+        console.log("Dziś jest czwartek");
+        break;
+    default:
+        console.log("Nie mam pojęcia jaki jest dzisiaj dzień :(");
+        break;
+}
